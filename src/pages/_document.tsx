b66a52b44d01095c540838c87ae2e6bd8adb588e
opera7133 <ainznino@pm.me>
2023-02-20 23:07:55 +0900

    ga4

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,23 +6,19 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        {GA_MEASUREMENT_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){window.dataLayer.push(arguments);}
             gtag('js', new Date());
 
             gtag('config', ${GA_MEASUREMENT_ID});
           `}
-            </Script>
-          </>
-        )}
+        </Script>
       </Head>
       <body className="relative">
         <Main />
