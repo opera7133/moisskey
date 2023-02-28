56500e5d3b0bf7a1e607a70144e2efbbe38fbcef
opera7133 <ainznino@pm.me>
2023-03-01 00:27:10 +0900

    error handling

--- src/pages/api/notes/urls.ts
+++ src/pages/api/notes/urls.ts
@@ -13,15 +13,22 @@ async function getByUrl(req: NextApiRequest, res: NextApiResponse) {
     if (!process.env.MIAUTH_KEY) return res.status(500).json({ status: "error", error: "key is not provided" })
     const notes = await Promise.all(req.body.urls.map(async (url: string) => {
       const urlReg = /(https:\/\/([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,})\/notes\/(.*)/
-      const apiURL = new URL("/api/notes/show", (url.match(urlReg) || [])[1])
-      const postData: PostData = { noteId: (url.match(urlReg) || [])[3] }
-      let note = await (await fetch(apiURL.toString(), { method: "POST", body: JSON.stringify(postData), headers: { "Content-Type": "application/json" } })).json()
-      if (note.renote && note.renote.text && !note.text) {
-        note = { ...note, renote: { ...note.renote, html: toHtml(parse(note.renote.text), { url: getHost(note.renote, (url.match(urlReg) || [])[1]) }), user: { ...note.renote.user, host: getHost(note.renote, (url.match(urlReg) || [])[1]) } } }
-      } else if (note.text) {
-        note = { ...note, html: toHtml(parse(note.text), { url: getHost(note, (url.match(urlReg) || [])[1]) }), user: { ...note.user, host: getHost(note, (url.match(urlReg) || [])[1]) } }
+      const params = url.match(urlReg)
+      if (params) {
+        const apiURL = new URL("/api/notes/show", params[1])
+        const postData: PostData = { noteId: params[3] }
+        let note = await (await fetch(apiURL.toString(), { method: "POST", body: JSON.stringify(postData), headers: { "Content-Type": "application/json" } })).json()
+        if (note.renote && note.renote.text && !note.text) {
+          note = { ...note, renote: { ...note.renote, html: toHtml(parse(note.renote.text), { url: getHost(note.renote, params[1]) }), user: { ...note.renote.user, host: getHost(note.renote, params[1]) } } }
+        } else if (note.text) {
+          note = { ...note, html: toHtml(parse(note.text), { url: getHost(note, params[1]) }), user: { ...note.user, host: getHost(note, params[1]) } }
+        } else {
+          throw new Error("ノートがありません")
+        }
+        return note
+      } else {
+        throw new Error("URLが正しくありません")
       }
-      return note
     }))
     return res.status(200).json({ status: "success", notes: notes })
   } catch (e) {
